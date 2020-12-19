2745a7d3dee27fc0aa0381e9a299f01ef10df77c
csharp-guru <csharp.guru@yandex.ru>
2020-12-19 20:56:17 +0300

    CSharo9.Features.TargetTypedNewExpressions

---
+++ CSharo9.Features.TargetTypedNewExpressions/ExampleObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharo9.Features.TargetTypedNewExpressions
+{
+    public class ExampleObject
+    {
+        public string PropertyString { get; set; }
+        public Dictionary<string, string> Dictionary { get; set; }
+
+        // initializing readonly or static types
+        private readonly static object s_syncObj = new();
+
+        // creating object's type is already known
+        public static ExampleObject InitExampleObject() => new();
+    }
+}
---
+++ CSharo9.Features.TargetTypedNewExpressions/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharo9.Features.TargetTypedNewExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // In C# 9.0, you can omit the type in a new expression when 
+            // the created object's type is already known. The most common 
+            // use is in field declarations:
+
+            // initializing object
+            ExampleObject exampleObject1 = new();
+
+            // combining initializing with properties 
+            ExampleObject exampleObject2 = new() { PropertyString = "bla bla bla" };
+
+            Dictionary<string, string> dictionary = new()
+            {
+                ["key1"] = "value1",
+                ["key2"] = "value2",
+            };
+
+            ExampleObject exampleObject3 = new()
+            {
+                PropertyString = "bla bla bla",
+                Dictionary = new()
+            };
+
+            // initializing generic types
+            List<ExampleObject> _listOfExampleObject = new();
+
+            // initializing tuples
+            (int a, int b) tuple1 = new(1, 2); // but "new" is redundant
+            // or delegates
+            Action action1 = new(() => { }); // but "new" is redundant
+
+            // initializing tuples with default values 
+            (int a, int b) tuple2 = new();
+
+            // BUT!!!
+            Action action2 = new(); // => Error CS1729. no constructor found
+
+            // 
+            // Error CS1729 'Action' does not contain a constructor 
+            // that takes 0 arguments.
+            // 
+
+        }
+    }
+}
+
